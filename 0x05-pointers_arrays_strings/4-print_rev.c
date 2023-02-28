1e9095edaf2bbcc585770a21289a2a405db956db
Mohamedsoula <mohamedsoula7@gmail.com>
2023-02-28 10:50:38 -0800

    M.S

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int lng = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		lng++;
+		s++;
+	}
+	s--;
+	for (o = lng; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
